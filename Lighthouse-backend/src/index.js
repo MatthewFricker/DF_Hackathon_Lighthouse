5537547c2f8822335cedeb93d2029d6f3727e8eb
Kenen <kenen1996@hotmail.co.uk>
2024-07-16 14:40:46 +0100

    backend setup

---
+++ Lighthouse-backend/src/index.js
@@ -0,0 +1,20 @@
+import Config from "./config/Config.js";
+import Database from "./db/Database.js";
+import AuthRoutes from "./routes/Auth.routes.js";
+import Server from "./server/Server.js";
+import Router from "./routes/Router.js";
+
+Config.load();
+const { PORT, HOST, DB_URI } = process.env;
+
+const router = new Router();
+const authRouter = new AuthRoutes();
+
+router.addRouter(authRouter);
+
+
+const server = new Server(PORT, HOST, router);
+const database = new Database(DB_URI);
+
+server.start();
+database.connect();
---
+++ Lighthouse-backend/src/services/Auth.service.js
@@ -0,0 +1,63 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.model.js";
+
+export default class AuthService {
+  register = async ({ username, password }) => {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = new User({ username, password: hashedPassword });
+      await user.save();
+      const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      return { user, accessToken };
+    } catch (error) {
+      throw new Error("Registration failed", error.message);
+    }
+  };
+
+  login = async ({ username, password }) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) throw new Error("User not found");
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) throw new Error("Invalid password");
+      const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      return { user, accessToken };
+    } catch (error) {
+      throw new Error("Login failed", error.message);
+    }
+  };
+
+  getUser = async (username) => {
+    try {
+      const user = await User.findOne({ username }).select("-password");
+      return user;
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+
+  // updateUser = async (username, data) => {
+  //   try {
+  //     const user = await User.findOneAndUpdate({ username }, data, {
+  //       new: true,
+  //     });
+  //     if (!user) {
+  //       console.log(`No user found with username: ${username}`);
+  //     }
+  //     return user;
+  //   } catch (e) {
+  //     console.error(`Error updating user: ${e}`);
+  //     throw new Error(e);
+  //   }
+  // };
+
+  // deleteUser = async (username) => {
+  //   try {
+  //     const user = await User.findOneAndDelete({ username });
+  //     return `${username} has been deleted`;
+  //   } catch (e) {
+  //     throw new Error(e);
+  //   }
+  // };
+}
