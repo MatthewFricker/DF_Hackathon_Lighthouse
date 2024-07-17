1a6d964a447fcd3752361b0a93d1794e59fbaac6
Kenen <kenen1996@hotmail.co.uk>
2024-07-17 21:38:03 +0100

    console log for testing

--- Lighthouse-backend/src/index.js
+++ Lighthouse-backend/src/index.js
@@ -5,14 +5,12 @@ import Server from "./server/Server.js";
 import Router from "./routes/Router.js";
 
 Config.load();
-const port =  process.env.PORT || 4000; //for deployment on render
-const {PORT, HOST, DB_URI } = process.env;
+const { PORT, HOST, DB_URI } = process.env;
 
 const router = new Router();
 const authRouter = new AuthRoutes();
 
 router.addRouter(authRouter);
-
 
 const server = new Server(PORT, HOST, router);
 const database = new Database(DB_URI);
