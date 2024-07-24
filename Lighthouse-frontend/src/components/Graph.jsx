79894795f51b9ef04f7a78378427ec9c8c7e525e
Kenen <kenen1996@hotmail.co.uk>
2024-07-24 19:14:06 +0100

    styling: point colour on graph

--- Lighthouse-frontend/src/components/Graph.jsx
+++ Lighthouse-frontend/src/components/Graph.jsx
@@ -193,7 +193,7 @@ const CreateGraph = (data, valueType, industry, modifiers) => {
       yScale(calculateValues(d, valueType).perceivedBusinessValue)
     )
     .attr("r", 5)
-    .attr("fill", "red")
+    .attr("fill", "blue")
     .style("cursor", "pointer")
     .on("mouseover", function (event, d) {
       d3.select(this).transition().attr("r", 7).attr("fill", "orange");
